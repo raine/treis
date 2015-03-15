2d896920f5dffb353eef44e554d7cb3a2e2dd5a8
Raine Virta <raine.virta@gmail.com>
2015-03-15 14:09:08 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+var util = require('util');
+var inspect = require('./lib/inspect')
+
+module.exports = function(fn) {
+  return function() {
+    var res = fn.apply(this, arguments);
+    console.log(inspect({
+      in: arguments,
+      out: res
+    }));
+
+    return res;
+  }
+};
