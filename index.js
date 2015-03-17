481d89624867b1dca227582496f5f448960521da
Raine Virta <raine.virta@gmail.com>
2015-03-17 20:45:09 +0200

    allow giving functions a name

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var inspect = require('./lib/inspect');
 
-module.exports = function(fn) {
+module.exports = function(name, fn) {
+  if (typeof name === 'function') {
+    fn   = name;
+    name = null;
+  }
+
   return function() {
     var res = fn.apply(this, arguments);
-    console.log(inspect({
+    var prefix = name ? name + ' ' : '';
+
+    console.log(prefix + inspect({
       in: arguments,
       out: res
     }));
