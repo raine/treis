5fcca472b32d23374505576d9aa2197f65ddc59f
Raine Virta <raine.virta@gmail.com>
2015-04-16 00:44:39 +0300

    clone arguments in case the function mutates its arguments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var inspect = require('./lib/inspect');
 var str2color = require('./lib/str2color');
+var clone = require('clone');
 
 module.exports = function(name, fn) {
   if (typeof name === 'function') {
@@ -8,11 +9,12 @@ module.exports = function(name, fn) {
   }
 
   return function(/* args */) {
+    var args = clone(arguments);
     var res = fn.apply(this, arguments);
     var prefix = name ? str2color(name) + ' ' : '';
 
     console.log(prefix + inspect({
-      in: arguments,
+      in: args,
       out: res
     }));
 
