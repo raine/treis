1e1b450ddef5caa49318e711095a30f3f0930e11
Raine Virta <raine.virta@gmail.com>
2015-04-16 22:26:20 +0300

    parse and display fn names and args

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 var inspect = require('./lib/inspect');
 var str2color = require('./lib/str2color');
-var clone = require('clone');
+var R = require('ramda');
+var chalk = require('chalk');
+var getFnArgs = require('./lib/get-fn-args');
 
 module.exports = function(name, fn) {
   if (typeof name === 'function') {
     fn   = name;
-    name = null;
+    name = fn.name || 'ƒ';
   }
 
+  var prefix = name ? str2color(name) + ' ' : '';
+
   return function(/* args */) {
-    var args = clone(arguments);
+    console.log(prefix + formatArgsStr(name, fn, arguments));
     var res = fn.apply(this, arguments);
-    var prefix = name ? str2color(name) + ' ' : '';
-
-    console.log(prefix + inspect({
-      in: args,
-      out: res
-    }));
-
+    console.log(prefix + '=> ' + inspect(res));
     return res;
   };
 };
+
+var repeatStr = R.compose(R.join(''), R.repeat);
+function formatArgsStr(name, fn, args) {
+  var fnArgNames = getFnArgs(fn);
+  var getArgPairs = function(i, val) {
+    return [ fnArgNames[i] || i, val ];
+  };
+
+  var pairs = R.zipWith(getArgPairs, R.range(0, args.length), args);
+  var lines = R.map(R.apply(formatArg), pairs);
+  var indentation = repeatStr(' ', name.length + 1);
+  var indentedTail = R.map(R.concat(indentation), R.tail(lines));
+  return R.join('\n', [ R.head(lines) ].concat(indentedTail) );
+
+  function formatArg(name, val) {
+    return R.join(': ', [ chalk.green(name), inspect(val) ]);
+  }
+}
