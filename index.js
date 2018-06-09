a15263c77ca93a8ca647ce13c28339c5ca55f5d9
Raine Virta <raine.virta@gmail.com>
2018-06-09 11:42:21 +0300

    format files with prettier

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-var R         = require('ramda');
-var chalk     = require('chalk');
-var utils     = require('./lib/utils');
-var str2color = require('./lib/str2color');
-var getFnArgs = require('./lib/get-fn-args');
-var getFnName = require('./lib/get-fn-name');
-var stripAnsi = require('strip-ansi');
-var inspect   = utils.inspect;
-var print     = utils.print;
+var R = require('ramda')
+var chalk = require('chalk')
+var utils = require('./lib/utils')
+var str2color = require('./lib/str2color')
+var getFnArgs = require('./lib/get-fn-args')
+var getFnName = require('./lib/get-fn-name')
+var stripAnsi = require('strip-ansi')
+var inspect = utils.inspect
+var print = utils.print
 
 var mapTail = function(fn, arr) {
-  return R.slice(0, 1, arr).concat(R.map(fn, R.tail(arr)));
-};
-var strRepeat = R.compose(R.join(''), R.repeat);
-var lines = R.split('\n');
-var unlines = R.join('\n');
+  return R.slice(0, 1, arr).concat(R.map(fn, R.tail(arr)))
+}
+var strRepeat = R.compose(R.join(''), R.repeat)
+var lines = R.split('\n')
+var unlines = R.join('\n')
 var indentTailLines = function(n, str) {
-  return unlines(mapTail(R.concat(strRepeat(' ', n)), lines(str)));
-};
+  return unlines(mapTail(R.concat(strRepeat(' ', n)), lines(str)))
+}
 
 var treis = function(__print, color) {
-  var fnNameGetter = getFnName();
-  var print = R.pipe(color ? R.identity : stripAnsi, __print);
+  var fnNameGetter = getFnName()
+  var print = R.pipe(color ? R.identity : stripAnsi, __print)
 
   return function(name, fn) {
     if (typeof name === 'function') {
-      fn = name;
-      name = null;
+      fn = name
+      name = null
     }
 
     return function(/* args */) {
-      if (name == null) name = fnNameGetter(fn);
-      name = name.toString();
+      if (name == null) name = fnNameGetter(fn)
+      name = name.toString()
 
-      var prefix = name ? str2color(name) + ' ' : '';
-      print(prefix + formatArgs(name, fn, arguments));
-      var res = fn.apply(this, arguments);
-      print(prefix + '=> ' + indentTailLines(name.length + 4, inspect(res)));
-      return res;
-    };
-  };
-};
+      var prefix = name ? str2color(name) + ' ' : ''
+      print(prefix + formatArgs(name, fn, arguments))
+      var res = fn.apply(this, arguments)
+      print(prefix + '=> ' + indentTailLines(name.length + 4, inspect(res)))
+      return res
+    }
+  }
+}
 
 module.exports = treis(print, true)
 module.exports.__init = treis
 
 function formatArgs(name, fn, args) {
-  var fnArgNames = getFnArgs(fn);
+  var fnArgNames = getFnArgs(fn)
   var getArgPairs = function(i, val) {
-    return [ fnArgNames[i] || i.toString(), val ];
-  };
+    return [fnArgNames[i] || i.toString(), val]
+  }
 
-  var pairs = R.zipWith(getArgPairs, R.range(0, args.length), args);
-  var argLines = R.map(R.apply(formatArg), pairs);
-  var space = strRepeat(' ', name.length + 1);
-  return unlines(mapTail(R.concat(space), argLines));
+  var pairs = R.zipWith(getArgPairs, R.range(0, args.length), args)
+  var argLines = R.map(R.apply(formatArg), pairs)
+  var space = strRepeat(' ', name.length + 1)
+  return unlines(mapTail(R.concat(space), argLines))
 
   function formatArg(argName, val) {
-    var indentLevel = name.length + argName.length + 3;
+    var indentLevel = name.length + argName.length + 3
     return R.join(': ', [
       chalk.green(argName),
       indentTailLines(indentLevel, inspect(val))
-    ]);
+    ])
   }
 }
